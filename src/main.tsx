79dc418f6bb6d41316823d76caed78f13e3b941d
Sebastian <sebastian.nowak@4-all.pl>
2024-03-09 15:04:44 +0100

    first commit with changes in tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,59 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+interface Experience {
+  description: string;
+  year: number;
+}
+
+interface CVData {
+  photo: string;
+  name: string;
+  position: string;
+  experience: Experience[];
+  education: string[];
+}
+
+const CvData: CVData = {
+  photo: 'https://sitenow.pl/images/sebastian.jpg',
+  name: 'Sebastian',
+  position: 'Front-end Developer',
+  experience: [{
+    description: 'Adimo', year: 2008
+  },
+  {
+    description: 'Agencja 4all', year: 2016
+  }],
+  education: ['coś tam', 'coś tam 2'],
+}
+
+const imgTag = (url: string, alt: string) => {
+  return <img src={url} alt={alt} />
+}
+
+const experienceLi = CvData.experience
+  .map(experience => (
+    <li key={experience.year}>
+      Rok: {experience.year} - {experience.description}
+    </li>
+  ));
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <h1>Yo!</h1>
+    <>
+      {imgTag(CvData.photo, `${CvData.name} ${CvData.position} `)}
+      {CvData.position}
+
+      <ul>
+        {experienceLi}
+      </ul>
+      <ul>
+        {CvData.education.map((education, index) =>
+          <li key={index}>{education};</li>
+        )}
+      </ul>
+    </>
+
   </React.StrictMode>,
 )
