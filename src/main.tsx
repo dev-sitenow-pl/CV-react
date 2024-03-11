cd13bf31326a15a2273a1c1d0e37bd39e3749f68
Sebastian <sebastian.nowak@4-all.pl>
2024-03-11 15:50:49 +0100

    add main.scss; end project

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './scss/main.scss'
 
 interface Experience {
   description: string;
@@ -9,6 +10,7 @@ interface Experience {
 interface CVData {
   photo: string;
   name: string;
+  lastName: string;
   position: string;
   experience: Experience[];
   education: string[];
@@ -17,14 +19,18 @@ interface CVData {
 const CvData: CVData = {
   photo: 'https://sitenow.pl/images/sebastian.jpg',
   name: 'Sebastian',
+  lastName: 'Nowak',
   position: 'Front-end Developer',
   experience: [{
     description: 'Adimo', year: 2008
   },
   {
+    description: 'Netivo', year: 2009
+  },
+  {
     description: 'Agencja 4all', year: 2016
   }],
-  education: ['coś tam', 'coś tam 2'],
+  education: ['Mega K', 'Mega K Frontowcy'],
 }
 
 const imgTag = (url: string, alt: string) => {
@@ -41,19 +47,50 @@ const experienceLi = CvData.experience
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <>
-      {imgTag(CvData.photo, `${CvData.name} ${CvData.position} `)}
-      {CvData.position}
+    <main>
+      <header className="box">
+        <h1>Curriculum vitae</h1>
+      </header>
 
-      <ul>
-        {experienceLi}
-      </ul>
-      <ul>
-        {CvData.education.map((education, index) =>
-          <li key={index}>{education};</li>
-        )}
-      </ul>
-    </>
+      <div className="content">
+        <div className="box box__left">
+          <div className="box__person-img">
+            {imgTag(CvData.photo, `${CvData.name} ${CvData.position} `)}
+            <div className="box__person-img__description">
+              <h2>{CvData.name} {CvData.lastName}</h2>
+              <h3>{CvData.position}</h3>
+            </div>
+
+          </div>
+
+        </div>
+        <div className="box box__right">
+          <div className="box__experience">
+            <h3>Doświadczenie zawodowe:</h3>
+            <ul>
+              {experienceLi}
+            </ul>
+          </div>
+          <div className="box__experience">
+            <h3>Edukacja:</h3>
+            <ul>
+              {CvData.education.map((education, index) =>
+                <li key={index}>{education},</li>
+              )}
+            </ul>
+          </div>
+
+
+        </div>
+      </div>
+      <footer className="box">
+        <h2>Więcej na <a href="http://sitenow.pl" target="_blank" rel="noopener noreferrer">sitenow.pl</a></h2>
+      </footer>
+
+
+
+
+    </main>
 
   </React.StrictMode>,
 )
